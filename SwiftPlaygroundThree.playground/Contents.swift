a6685390016dbb5e87f0a3013db58c3e22aabf52
kingregi1 <59853870+kingregi1@users.noreply.github.com>
2020-02-18 10:18:49 -1000

    Part 7
    Start Own your own

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
 var str = "Hello, playground"
+var fruitnames :[String]
+
+// Part Theee
+
+fruitnames = ["Banananas", "Apple", "Mangoes"]
+
+print (fruitnames)
+
+//Part 5
+
+//Interpolation
+print("I like to eat\(fruitnames)")
+
+for name in fruitnames{
+    print("I like to eat " + name)
+}
+print(fruitnames[2])
